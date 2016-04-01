29d601567d33a09b2866436c9263c2c811d47ea6
Bogdan <bogdan.lebedenko@gmail.com>
2016-04-01 19:00:34 +0300

    Tab functionality started

--- main.js
+++ main.js
@@ -14,32 +14,24 @@ todoApp.controller('tabSectionController', function($scope) {
 
 	$scope.tabs = [
 		{
-			"title": "Home",
+			"title": "AngularJS",
 			"content": "Raw denim you probably haven't heard of them jean shorts Austin. Nesciunt tofu stumptown aliqua, retro synth master cleanse. Mustache cliche tempor, williamsburg carles vegan helvetica."
 		},
 		{
-			"title": "Profile",
+			"title": "JavaScript",
 			"content": "Food truck fixie locavore, accusamus mcsweeney's marfa nulla single-origin coffee squid. Exercitation +1 labore velit, blog sartorial PBR leggings next level wes anderson artisan four loko farm-to-table craft beer twee."
 		},
 		{
-			"title": "About",
+			"title": "Тестирование",
 			"content": "Etsy mixtape wayfarers, ethical wes anderson tofu before they sold out mcsweeney's organic lomo retro fanny pack lo-fi farm-to-table readymade.",
 			"disabled": false
 		},
 		{
-			"title": "Contact",
+			"title": "Регулярные выражения",
 			"content": "Reprehenderit butcher retro keffiyeh dreamcatcher synth. Cosby sweater eu banh mi, qui irure terry richardson ex squid."
 		},
 		{
-			"title": "Contact",
-			"content": "Reprehenderit butcher retro keffiyeh dreamcatcher synth. Cosby sweater eu banh mi, qui irure terry richardson ex squid."
-		},
-		{
-			"title": "Contact",
-			"content": "Reprehenderit butcher retro keffiyeh dreamcatcher synth. Cosby sweater eu banh mi, qui irure terry richardson ex squid."
-		},
-		{
-			"title": "Contact",
+			"title": "В планах",
 			"content": "Reprehenderit butcher retro keffiyeh dreamcatcher synth. Cosby sweater eu banh mi, qui irure terry richardson ex squid."
 		}
 	];

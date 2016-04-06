d6161a1002982fce8cbb455b9340dcd2a5b73758
Bogdan <bogdan.lebedenko@gmail.com>
2016-04-06 23:56:33 +0300

    In process of working with ui-router module.

--- main.js
+++ main.js
@@ -1,14 +1,93 @@
 var todoApp = angular.module('todoApp', [
-		'ngAnimate',
-		'mgcrea.ngStrap',
-		'mgcrea.ngStrap.tab',
-		'ui.router'
+							 'ngAnimate',
+							 'mgcrea.ngStrap',
+							 'mgcrea.ngStrap.tab',
+							 'ui.router'
 ]);
 
+todoApp.config(function($stateProvider, $urlRouterProvider) {
+
+	$stateProvider
+			.state('mainpage',{
+				url : '/',
+				views : {
+					main : {
+						templateUrl: 'templates/main.html',
+						controller : 'ToDoCtrl'
+					}
+				}
+			})
+			.state('angular',{
+				url : 'angularjs',
+				views : {
+					'main' : {
+						templateUrl: 'templates/angularjs.html',
+						controller: 'angularConrtoller'
+					}
+				}
+			})
+			.state('testing', {
+				url : 'testing',
+				views : {
+					'main' : {
+						templateUrl: 'templates/testing.html',
+						controller: 'testController'
+					}
+				}
+			})
+			.state('javascript',{
+				url : 'js',
+				views : {
+					main : {
+						templateUrl: 'templates/js.hmtl',
+						controller: 'jsController'
+					}
+				}
+			})
+			.state('regexp',{
+				url: 'regexp',
+				views : {
+					main : {
+						templateUrl: 'templates/regexp.html',
+						controller: 'regexpController'
+					}
+				}
+			})
+			.state('plans',{
+				url: 'plans',
+				views : {
+					main : {
+						templateUrl: 'templates/plans.html',
+						controller: 'plansController'
+					}
+				}
+			});
+
+	$urlRouterProvider.otherwise('/');
+});
+
 todoApp.controller('ToDoCtrl', function($scope){
-
 	$scope.todo = model;
+});
 
+todoApp.controller('angularConrtoller', function($scope){
+	console.log('I\'m here - angular.');
+});
+
+todoApp.controller('testController', function($scope){
+	console.log('I\'m here - tests.');
+});
+
+todoApp.controller('jsController', function($scope){
+	console.log('I\'m here - js.');
+});
+
+todoApp.controller('regexpController', function($scope){
+	console.log('I\'m here - regexp.');
+});
+
+todoApp.controller('plansController', function($scope){
+	console.log('I\'m here - plans.');
 });
 
 var model = {
